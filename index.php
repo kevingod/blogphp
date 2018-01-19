b8ab90d2bad744aa7210dda0e5e03e4de0937413
guodong <gaoguodong@yunfan.com>
2018-01-19 14:51:31 +0800

    1.提交代码

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <meta charset="utf-8">
 <?php
 
+require('./lib/init.php');
+
+//获得所有文章列表
+$art_array = getAllArt();
+
+
 //显示主页
 include('./view/front/index.html');
+
+
+
+//获得所有的文章
+function getAllArt(){
+
+	$sql = sprintf("select * from art order by art_id asc ");
+
+	$rs =  mGetAll($sql);
+
+	return $rs;
+}
+
 
 ?>
